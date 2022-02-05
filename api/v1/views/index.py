746b4a38e23c9b0feb3479a95e216d1a7d0608bf
Juanse1595 <3309@holberstonschool.com>
2022-02-05 16:42:23 -0500

    ADD new method def count_objs() to api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """index module"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def status_check():
     """Returns the status of the app"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def count_objs():
+    """Returns the count of objects"""
+    result = {}
+    classes = {'amenities': 'Amenity', 'cities': 'City', 'places': 'Place',
+               'reviews': 'Review','states': 'State', 'users': 'User'}
+    for key, value in classes.items():
+        result[key] = storage.count(value)
+    return jsonify(result)
+    
