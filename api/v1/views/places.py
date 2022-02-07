c1268ef4a783c0aed9735a610e3d0a8d56ec6932
TheMasterTz <palavras2001@hotmail.com>
2022-02-06 20:45:28 -0500

    correction

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -56,26 +56,26 @@ def DELETE_place_id(place_id=None):
 def POST_places(city_id):
     """creates a city only using the name
     and returns it as a dictionary"""
-    dictionary = request.get_json()
-    if dictionary is None:
-        abort(400, 'Not a JSON')
-
-    city = storage.get('City', city_id)
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    if dictionary.get('user_id') is None:
-        abort(400, 'Missing user_id')
-
-    user = storage.get('User', dictionary.get('user_id'))
-    if user is None:
-        abort(404)
-    if dictionary.get('name') is None:
-        abort(400, 'Missing name')
-
-    dictionary['city_id'] = city_id
-    place = Place(**dictionary)
-    place.save()
-    return jsonify(place.to_dict()), 201
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    elif not data.get('user_id'):
+        abort(400, "Missing name")
+    else:
+        user = storage.get(User, data['user_id'])
+        if user is None:
+            abort(404)
+        elif not data.get('name'):
+            abort(404, "Missing name")
+        else:
+            data['city_id'] = city_id
+            NewPlace = Place(**data)
+            storage.new(NewPlace)
+            NewPlace.save()
+            return jsonify(NewPlace.to_dict()), 201
 
 
 @app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
